d6c4be03641ff332973c13462e05492db992c59f
cashmerewool <67176000+cashmerewool@users.noreply.github.com>
2023-01-18 14:44:59 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -63,4 +63,17 @@ class SearchEngine implements URLHandler {
         return "Invalid Path";
 
     }
+
+    class SearchServer {
+        public static void main(String[] args) throws IOException {
+            if(args.length == 0){
+                System.out.println("Missing port number! Try any number between 1024 to 49151");
+                return;
+            }
+    
+            int port = Integer.parseInt(args[0]);
+    
+            Server.start(port, new Handler());
+        }
+    }
 }
