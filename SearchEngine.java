d79895d969938f47c94d15ab336f5e48c59eda0d
cashmerewool <67176000+cashmerewool@users.noreply.github.com>
2023-01-18 14:46:58 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
-class SearchEngine implements URLHandler {
+class Search implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     ArrayList<String> list = new ArrayList<>();
@@ -63,17 +63,17 @@ class SearchEngine implements URLHandler {
         return "Invalid Path";
 
     }
+}
 
-    class SearchServer {
-        public static void main(String[] args) throws IOException {
-            if(args.length == 0){
-                System.out.println("Missing port number! Try any number between 1024 to 49151");
-                return;
-            }
-    
-            int port = Integer.parseInt(args[0]);
-    
-            Server.start(port, new Handler());
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
         }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
     }
 }
