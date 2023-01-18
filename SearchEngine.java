b530c534c52f930d80ccc67fe144e372ce54d45e
cashmerewool <67176000+cashmerewool@users.noreply.github.com>
2023-01-18 14:42:53 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class SearchEngine implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> list = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+
+        //add
+        if (url.getPath().equals("/add")) {
+            try {
+                String[] query = url.getQuery().split("=");
+                String word = query[1];
+                list.add(word);
+                return "Successfully Added!";
+            } catch(Exception E) {
+                return "Unsuccessful. Syntax: /add?s=[word]";
+            }
+
+        }
+        //search
+        if (url.getPath().equals("/search")) {
+            ArrayList<String> findings = new ArrayList<>();
+            try {
+                String[] query = url.getQuery().split("=");
+                String word = query[1];
+                
+                for (int i = 0; i < list.size(); i++) {
+                    String term = list.get(i);
+                    //found word
+                    if (word.equals(term)) {
+                        findings.add(term);
+                    }
+                    //skip if searched word is too big
+                    if (word.length() > term.length()) {
+                        continue;
+                    } else {
+                        //find
+                        //pineapple
+                        //app
+                        for (int j = 0; j < term.length()-word.length(); j++) {
+                            if (word.equals(term.substring(i, i + word.length()))) {
+                                findings.add(term);
+                            }
+                        }
+                    }
+
+                    if (findings.size() == 0) {
+                        return "Nothing Found.";
+                    } else {
+                        return findings.toString();
+                    }
+                }
+            } catch (Exception E) {
+                return "ERROR";
+            }
+        }
+
+        //wrong path
+        return "Invalid Path";
+
+    }
+}
